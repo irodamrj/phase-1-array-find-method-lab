6d157e04f6683d2d4c5b66bd311fe8e2061e9718
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-25 01:48:35 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record){
+    const winObject =  record.find(item => item.result === "W");
+    if(winObject != undefined )
+        return winObject.year;
+    return winObject;
+}
